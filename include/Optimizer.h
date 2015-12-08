4d87e9c7e6a7179890ea0779df62d981b7781a06
jingpang <jp08@foxmail.com>
2015-12-09 02:22:11 +0800

    add tmp localBA function 151209

--- include/Optimizer.h
+++ include/Optimizer.h
@@ -49,6 +49,10 @@ public:
 
 
     static int OptimizeSim3(KeyFrame* pKF1, KeyFrame* pKF2, std::vector<MapPoint *> &vpMatches1, g2o::Sim3 &g2oS12, float th2 = 10);
+
+	//Added by wangjing
+    void static LocalBundleAdjustmentWJ(KeyFrame* pKF, bool *pbStopFlag=NULL);
+	//
 };
 
 } //namespace ORB_SLAM
